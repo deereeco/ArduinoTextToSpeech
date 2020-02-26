7b39cec3338aefcc1c2bf1bbed1bcb02adf655c1
deereeco <37238410+deereeco@users.noreply.github.com>
2020-02-26 12:58:01 -0500

    Add files via upload
    added instructions

--- example.ino
+++ example.ino
@@ -1,3 +1,74 @@
+/*
+Source:
+https://www.instructables.com/id/TukuTuku-Voice-Recognition/
+
+Software to program voice recognition module:
+https://learn.sparkfun.com/tutorials/terminal-basics/coolterm-windows-mac-linux
+
+Instructions to use software:
+Connections(from FTDI to voice module):
+TX - RX
+RX- TX
+VCC- VCC
+GND -GND
+
+
+open access port
+- select baud rate - 9600
+- Select - com port
+
+
+Type Hex command :
+
+At the beginning you need to select
+
+AA 36 - to enter into common mode
+
+then to record first group ,type
+
+AA 11 - to start recording first group
+
+
+whenever it says :-
+
+start - you need to speak command ,
+
+again- wait
+
+different- wait
+
+finish one -means command is recorded
+
+repeat this for 4times - give different commands
+
+finished group one - 5commands is recorded
+
+after this type -
+AA - 21 to import group one
+
+
+
+if you want to record second group
+
+type command
+
+AA 12
+AA 22 to import group 2
+
+AA 24 - query
+
+AA 01 to delete group1
+AA 02 to delete group2
+
+Connections:
+TX-RX
+RX-TX
+VCC-VCC
+GND -GND
+
+and LED as per the diagram!!
+*/
+
 int redPin = 9;
 int greenPin = 10;
 int bluePin = 11; byte
